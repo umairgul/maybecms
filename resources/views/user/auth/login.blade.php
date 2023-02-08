c747726d881090311e2877ee3ffe6eedb0299b08
Umair Gul <umairguluol@gmail.com>
2023-02-08 19:07:46 +0500

    daisy ui

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -1,4 +1,7 @@
 @extends('user.layouts.app')
 
 @section('content')
+<div class="h-screen w-full flex items-center justify-center">
+    <button class="btn btn-primary">Login</button>
+</div>
 @endsection
